d4deabeefc3d0db8ada1c817e411f2215d57da98
OswaldoRo <116927565+OswaldoRo@users.noreply.github.com>
2024-11-02 23:49:15 -0500

    mostrar matriz de 4 posiciones

--- Ejemplo2_javagit.java
+++ Ejemplo2_javagit.java
@@ -8,10 +8,14 @@ public class Ejemplo2_javagit {
 		
 		int [] numeros = new int[4];
 		
+		numeros [0]=1;
+		numeros [1]=2;
+		numeros [2]=3;
+		numeros [3]=4;
 		
-		
-		
-		
+		for(int i =0;i<numeros.length;i++) {
+			System.out.println(numeros[i]);
+		}
 
 	}
 
