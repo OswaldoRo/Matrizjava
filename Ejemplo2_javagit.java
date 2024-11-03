5a75dc77f08f72e2d75a732f8a7f89d8cae0f396
OswaldoRo <116927565+OswaldoRo@users.noreply.github.com>
2024-11-02 23:25:51 -0500

    Cambio 1

--- Ejemplo2_javagit.java
+++ Ejemplo2_javagit.java
@@ -3,6 +3,10 @@ public class Ejemplo2_javagit {
 
 	public static void main(String[] args) {
 		System.out.println("!!    Mi proyecto   !!");
+		
+		System.out.println("!!    Cambio 1   !!");
+		
+		
 
 	}
 
